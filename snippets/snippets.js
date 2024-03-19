db052c83ce7916d7c04978816ea7ffc69f57b675
DanOcho8 <98377769+DanOcho8@users.noreply.github.com>
2024-03-18 22:48:42 -0700

    added functionality to snippets and new html file to display information

--- snippets/snippets.js
+++ snippets/snippets.js
@@ -1,108 +1,21 @@
 
-const sleepLink = document.getElementById('sleepLink');
-const scalpLink = document.getElementById( 'scalpLink' );
-const hairThinLink = document.getElementById('hairThinLink');
-const scalpPsoriasisLink = document.getElementById('scalpPsoriasisLink');
-const dandruffLink = document.getElementById('dandruffLink');
-const scalpInfectionLink = document.getElementById('scalpInfectionLink');
-const baldSpotLink = document.getElementById('baldSpotsLink');
+document.addEventListener('DOMContentLoaded', () => {
+    //get the query param
+    const params =  new URLSearchParams(window.location.search);
+    const selectedBlogClass = params.get('blog');
 
-sleepLink.addEventListener('click', function(event) {
-    event.preventDefault();
-    console.log('Clicked Sleep Link');
-    renderBlogContent('sleep');
-});
-scalpLink.addEventListener('click', function(event) {
-    event.preventDefault();
-    renderBlogContent('scalpHealth');
-});
-hairThinLink.addEventListener('click', function(event) {
-    event.preventDefault();
-    renderBlogContent('hairThinning');
-});
-scalpPsoriasisLink.addEventListener('click', function(event) {
-    event.preventDefault();
-    renderBlogContent('scalpPsoriasis');
-});
-dandruffLink.addEventListener('click', function(event) {
-    event.preventDefault();
-    renderBlogContent('dandruff');
-});
-scalpInfectionLink.addEventListener('click', function(event) {
-    event.preventDefault();
-    renderBlogContent('scalpInfections');
-});
-baldSpotLink.addEventListener('click', function(event) {
-    event.preventDefault();
-    renderBlogContent('baldSpots');
+    activateBlog(selectedBlogClass);
 });
 
-function renderBlogContent(blogType){
-    let blogContentContainer = document.getElementById('blogContentContainer');
-
-    if (!blogContentContainer){
-        blogContentContainer = document.createElement('div');
-        blogContentContainer.id = 'blogContentContainer';
-        document.body.appendChild(blogContentContainer);
+function activateBlog(selectedBlogClass) {
+    document.querySelectorAll('.blog-section').forEach(section=>{
+        section.style.display='none';
+    });
+    const selectedBlog = document.querySelector(`.${selectedBlogClass}`);
+    if (selectedBlog){
+        selectedBlog.style.display = 'block';
     }
-
-
-    switch (blogType){
-        case 'sleep':
-            blogContentContainer.innerHTML = `
-                <div>
-                    <h2>Importance Of Sleep</h2>
-                    <p>Often neglected due to our busy lives is sleep, here’s a few suggestions for a healthy sleep schedule for men. It's important to note that while individual sleep needs can vary, here’s some key points to keep in mind</p>
-                </div>
-            `;
-            break;
-        case 'scalpHealth':
-            blogContentContainer.innerHTML = `
-                <div>
-                    <h2>Scalp Health</h2>
-                    <p>Men to ensure great scalp health, it's important to use products that promote a balanced and healthy scalp environment. Here are some types of products that can contribute to scalp health</p>
-                </div>
-            `;
-            break;
-        case 'hairThinning':
-            blogContentContainer.innerHTML = `
-                <div>
-                    <h2>Scalp Health</h2>
-                    <p>Men to ensure great scalp health, it's important to use products that promote a balanced and healthy scalp environment. Here are some types of products that can contribute to scalp health</p>
-                </div>
-            `;
-            break;
-        case 'scalpPsoriasis':
-            blogContentContainer.innerHTML = `
-                <div>
-                    <h2>Scalp Health</h2>
-                    <p>Men to ensure great scalp health, it's important to use products that promote a balanced and healthy scalp environment. Here are some types of products that can contribute to scalp health</p>
-                </div>
-            `;
-            break;
-        case 'dandruff':
-            blogContentContainer.innerHTML = `
-                <div>
-                    <h2>Scalp Health</h2>
-                    <p>Men to ensure great scalp health, it's important to use products that promote a balanced and healthy scalp environment. Here are some types of products that can contribute to scalp health</p>
-                </div>
-            `;
-            break;
-        case 'scalpInfections':
-            blogContentContainer.innerHTML = `
-                <div>
-                    <h2>Scalp Health</h2>
-                    <p>Men to ensure great scalp health, it's important to use products that promote a balanced and healthy scalp environment. Here are some types of products that can contribute to scalp health</p>
-                </div>
-            `;
-            break;
-        case 'baldSpots':
-            blogContentContainer.innerHTML = `
-                <div>
-                    <h2>Sca</h2>
-                    <p>Men to ensure great scalp health, it's important to use products that promote a balanced and healthy scalp environment. Here are some types of products that can contribute to scalp health</p>
-                </div>
-            `;
-            break;
+    else{
+        console.error('Could not find a blog');
     }
 }
