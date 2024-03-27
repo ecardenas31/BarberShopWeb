ccdd2f44e6a5d993ca519a84c9bbb521acbcf86c
DanOcho8 <98377769+DanOcho8@users.noreply.github.com>
2024-03-26 20:24:58 -0700

    added some more changed to the chatbot, need to fix it though

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
 from flask import Flask, render_template, request, jsonify
-
+from flask_cors import CORS
 from chat import get_response
 
 app = Flask(__name__)
-
-
-@app.get("/")
-def index_get():
-    return render_template("chatbot.html")
+CORS(app)
 
 @app.post("/predict")
 def predict():
     text = request.get_json().get("message")
+    print("Received message:", text)
     response = get_response(text)
     message = {"answer": response}
     return jsonify(message)
