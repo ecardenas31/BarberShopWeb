6f08dd556b15d00b56a3a2b7403228a9d3d584a7
DanOcho8 <98377769+DanOcho8@users.noreply.github.com>
2024-04-02 15:47:19 -0700

    working changes

--- FirebaseProject/app.py
+++ FirebaseProject/app.py
@@ -3,16 +3,19 @@ from flask_cors import CORS
 from chat import get_response
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/predict": {"origins": "https://127.0.0.1:5500"}})
 
-@app.post("/predict")
+@app.route("/")
+def index_get():
+    return render_template("chatbot.html")
+
+@app.route("/predict", methods=["POST"])
 def predict():
     text = request.get_json().get("message")
     print("Received message:", text)
     response = get_response(text)
     message = {"answer": response}
     return jsonify(message)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
