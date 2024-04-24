95c974aedfc681b4b5e8d4be1a4521d5550b1011
ecardenas31 <ecardenas31@outlook.com>
2024-04-23 22:04:39 -0700

    database

---
+++ FirebaseProject/firebase.js
@@ -0,0 +1,17 @@
+// firebase.js
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBQixGMlU8d0lgtSJPp3ZR_f5nF1wqsB6k",
+    authDomain: "barberowlsandwolves.firebaseapp.com",
+    databaseURL: "https://barberowlsandwolves-default-rtdb.firebaseio.com",
+    projectId: "barberowlsandwolves",
+    storageBucket: "barberowlsandwolves.appspot.com",
+    messagingSenderId: "268304668095",
+    appId: "1:268304668095:web:63e5e1665cf41242bbfd0a",
+    measurementId: "G-ZV779VSLF9"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
